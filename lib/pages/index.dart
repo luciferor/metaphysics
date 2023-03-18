d006e972a495ec4a5a7b6e53b4610d9ebf75bd6b
学生 <root@dsnbc.com>
2023-03-18 21:08:45 +0800

    changes index

--- lib/pages/index.dart
+++ lib/pages/index.dart
@@ -4,17 +4,21 @@ class Homie extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(100, 255, 255, 255),
+      backgroundColor: Color.fromARGB(99, 0, 0, 0),
       body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            Container(
+              child: Image(image: AssetImage('assets/images/logo.png')),
+              height: 150,
+              width: 150,
+            ),
             Center(
-              child: Text('Hello Flutter!',
+              child: Text('缔造亚洲智能软件',
                   style: TextStyle(
                     fontSize: 30,
                     color: Colors.white,
-                    decoration: TextDecoration.underline,
                   )),
             ),
           ]),
